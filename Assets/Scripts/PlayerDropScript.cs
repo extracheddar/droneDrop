e35990650d76f40a6b15baa3de42c6beb35f723a
James Spence <Jamesaspence20@gmail.com>
2017-12-02 10:48:07 -0600

    cleaned up code, removed logs

--- Assets/Scripts/PlayerDropScript.cs
+++ Assets/Scripts/PlayerDropScript.cs
@@ -10,17 +10,11 @@ public class PlayerDropScript : MonoBehaviour
 	public float forwardSpeed;
 	public int rateOfDrop;
 	private DateTime lastDropped;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			Debug.Log("Key down!");
 			if (canDropAnotherPackage())
 			{
 				dropPackage();
@@ -31,7 +25,6 @@ public class PlayerDropScript : MonoBehaviour
 	void dropPackage () {
 		GameObject package = Instantiate(packagePrefab, packageSpawn.position, packageSpawn.rotation);
 		Package packageScript = (Package) package.GetComponent(typeof(Package));
-		Debug.Log(packageScript);
 		packageScript.forwardThrust = forwardSpeed;
 		Destroy(package, 10f);
 		lastDropped = DateTime.Now;
