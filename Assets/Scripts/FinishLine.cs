7cc816d648e2f8b2739968df35dd45a62af6d51c
James Spence <Jamesaspence20@gmail.com>
2017-12-02 16:27:59 -0600

    new finish line in main, updated main to be like tests

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{	
+	private void OnTriggerEnter (Collider other) {
+		Debug.Log("Ending game!");
+		//TODO stop game via GameController
+		CommonObjects.GetThrust().DisableThrust();
+	}
+}
