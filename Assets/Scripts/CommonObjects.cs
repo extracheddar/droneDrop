9a09f5977c4d20ea9e357e61792d08ea13c4e752
James Spence <Jamesaspence20@gmail.com>
2017-12-02 16:27:29 -0600

    new CommonObjects class to retrieve instances

---
+++ Assets/Scripts/CommonObjects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class CommonObjects
+{
+    private static GameController gameController;
+    private static GameObject player;
+    private static Thrust playerThrust;
+
+    public static GameController GetGameController () {
+        return gameController ?? (gameController = GameObject.FindGameObjectWithTag(TagConstants.GAME_CONTROLLER)
+                   .GetComponent<GameController>());
+    }
+    
+    public static GameObject GetPlayer () {
+        return player ?? (player = GameObject.FindGameObjectWithTag(TagConstants.PLAYER));
+    }
+
+    public static Thrust GetThrust () {
+        return playerThrust ?? (playerThrust = GetPlayer().GetComponent<Thrust>());
+    }
+
+}
--- Assets/Scripts/PackageCollision.cs
+++ Assets/Scripts/PackageCollision.cs
@@ -2,21 +2,13 @@ using UnityEngine;
 
 public class PackageCollision : MonoBehaviour
 {
-	
-	private GameController gameController;
-
 	public int points;
-
-	private GameController GetGameController () {
-		return gameController ?? (gameController = GameObject.FindGameObjectWithTag(TagConstants.GAME_CONTROLLER)
-			       .GetComponent<GameController>());
-	}
 
 	private void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag(TagConstants.PACKAGE)) {
 			other.gameObject.tag = TagConstants.UNTAGGED;
-			GetGameController ().UpdateScore (points);
+			CommonObjects.GetGameController().UpdateScore (points);
 		}
 	}
 
