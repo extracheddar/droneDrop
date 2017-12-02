fb762eea1533721eabf71e97a7c7dff7bb79deb8
James Spence <Jamesaspence20@gmail.com>
2017-12-02 14:53:05 -0600

    lots can be enabled / disabled

---
+++ Assets/Scripts/Lot.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lot : MonoBehaviour
+{
+	public bool dropZoneEnabled;	
+}
--- Assets/Scripts/PackageCollision.cs
+++ Assets/Scripts/PackageCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PackageCollision : MonoBehaviour
 {
 	private GameController gameController;
+	private Lot lot;
 
 	public int points;
 
@@ -12,9 +13,22 @@ public class PackageCollision : MonoBehaviour
 			       .GetComponent<GameController>());
 	}
 
+	private Lot GetLot () {
+		return lot ?? (lot = GetComponentInParent<Lot>());
+	}
+
 	private void OnTriggerEnter(Collider other) 
 	{
+		if (!LotIsEnabled())
+		{
+			return;
+		}
+		
 		GetGameController().AddScore(points);
 		Debug.Log(GetGameController().GetScore());
+	}
+
+	private bool LotIsEnabled () {
+		return GetLot().dropZoneEnabled;
 	}
 }
