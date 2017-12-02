1f094163126e3d88d562f569beb553b0367e1f5f
James Spence <Jamesaspence20@gmail.com>
2017-12-02 00:58:15 -0600

    added new scripts, plugin dir

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public float offset = 0f;
+
+    private GameObject player;
+
+    void Start () {
+        player = GameObject.FindGameObjectWithTag ("Player");
+    }
+
+    void Update () {
+		
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = new Vector3 (transform.position.x, transform.position.y, player.transform.position.z + offset);
+    }
+
+}
---
+++ Assets/Scripts/Thrust.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float latSpeed = 5f;
+    public float forwardSpeed = 10f;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        float right = 0f;
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            right = -1f;
+        } else if (Input.GetKey (KeyCode.RightArrow)) {
+            right = 1f;
+        }
+        Vector3 movement = new Vector3 (right * latSpeed, 0.0f, forwardSpeed);
+        rb.AddForce (movement);
+    }
+}
