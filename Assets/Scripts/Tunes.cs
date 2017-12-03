ef7f0a089472f6de1752d343376f6c6952dde3da
Aaron Zaffke <zaffkea@Aarons-MacBook-Pro.local>
2017-12-03 17:15:36 -0600

    tunes

---
+++ Assets/Scripts/Tunes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tunes : MonoBehaviour {
+
+	void Awake() {
+//		DontDestroyOnLoad(transform.gameObject);
+
+		DontDestroyOnLoad(this);
+
+		if (FindObjectsOfType (GetType ()).Length > 1) {
+			Destroy (gameObject);
+		} else {
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+
+}
