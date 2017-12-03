144a3b77bdd694c0de48e1177b3c5e545c193228
James Spence <Jamesaspence20@gmail.com>
2017-12-02 18:10:35 -0600

    bounds on user

--- Assets/Scripts/Thrust.cs
+++ Assets/Scripts/Thrust.cs
@@ -1,23 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Win32;
 using UnityEngine;
 
-public class Thrust : MonoBehaviour {
+public class Thrust : MonoBehaviour
+{
 
-    private Rigidbody rb;
-
+    public GameObject ground;
     public float latSpeed = 5f;
     public float forwardSpeed = 10f;
+    
+    private Rigidbody rb;
     private bool generatingThrust = true;
+    private float minBounds;
+    private float maxBounds;
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+        Renderer sizeRenderer = ground.GetComponent<Renderer>();
+        minBounds = sizeRenderer.bounds.min.x + 5;
+        maxBounds = sizeRenderer.bounds.max.x - 5;
     }
 
     void Update () {
 		if (generatingThrust && Time.timeScale == 1)
         {
             rb.AddForce (GetMovement());            
+        }
+    }
+
+    void FixedUpdate () {
+        float zVal = rb.position.z;
+        float xVal = rb.position.x;
+        
+        rb.position = new Vector3 
+        (
+            Mathf.Clamp (xVal, minBounds, maxBounds), 
+            rb.position.y,
+            zVal
+        );
+
+        if (xVal <= minBounds || xVal >= maxBounds)
+        {
+            rb.velocity = new Vector3
+            (
+                0,
+                rb.velocity.y
+            );
         }
     }
 
